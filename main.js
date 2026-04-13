21adb41a230622b22c30e9b43d38f255b8f2a2db
videy-web <becakturbo07@gmail.com>
2026-04-14 01:38:56 +0700

    Update main.js

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const shareBtn = document.getElementById("shareBtn");
 const shareLinks = document.getElementById("shareLinks");
 
 shareBtn.onclick = () => {
-    window.open("https://heylink.me/selisih2rb/", "_blank")
+    window.open("https://ln.run/new_member/", "_blank")
 };
 /* UNDANGAN */
 function joinWhatsAppGroup() {
@@ -11,7 +11,7 @@ function joinWhatsAppGroup() {
 }
 
 function openFacebookPage() {
-    window.open("https://t.me/blunderviral18");
+    window.open("https://t.me/vidiovirall123");
 }
 const video = document.getElementById("video");
 const overlay = document.getElementById("videoOverlay");
@@ -32,7 +32,7 @@ overlay.addEventListener("click", () => {
     overlay.classList.remove("show");
 
     // Aksi setelah klik
-    window.open("https://heylink.me/selisih2rb/", "_blank");
+    window.open("https://ln.run/new_member/", "_blank");
 });
 
 
